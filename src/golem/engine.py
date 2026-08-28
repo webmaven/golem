dd280b3837320485b24eca329e4781c364076b20
Michael R. Bernstein <zopemaven@gmail.com>
2026-08-27 21:21:51 -0600

    docs(engine): update get_file_metadata docstring for layout classes

--- src/golem/engine.py
+++ src/golem/engine.py
@@ -481,7 +481,7 @@ class BuildEngine:
         `path` (Path):: Path to the target AsciiDoc document.
 
         [returns]
-        `dict[str, Any]`:: Dictionary containing `"title"`, `"nav_title"`, `"nav_order"`, and `"has_toc"` keys.
+        `dict[str, Any]`:: Dictionary containing `"title"`, `"nav_title"`, `"nav_order"`, `"has_toc"`, `"page_class"`, `"body_class"`, and `"content_class"` keys.
         """
         p_abs = str(path.resolve())
         current_hash = self._get_sha256(path)
